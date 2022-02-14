3084e288e013848638909f92c3822c2814ac32c1
Heidi <hlmunson@hotmail.com>
2022-02-14 16:22:56 -0700

    first commit

---
+++ greed/game/casting/score.py
@@ -0,0 +1,11 @@
+from game.casting.actor import Actor
+
+
+class Score(Actor):
+    """
+    The responsibility of Score is to keep track of the score.
+    Attributes:
+        
+    """
+    def __init__(self):
+        super().__init__()
